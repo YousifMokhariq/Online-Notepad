25115998815e9bb365c132a6402904fefae8628f
Yousef <yousefmokhariq@gmail.com>
2025-04-11 18:28:05 +0300

    added the form to write notes + some changes on the header

--- index.php
+++ index.php
@@ -2,8 +2,15 @@
 <html lang="en">
 
 <?php require('template/header.php') ?>
-<div class="justify-center items-center h-screen">
-    <textarea></textarea>
+
+<div class="flex justify-center items-center mt-10">
+    <form action="" method="post">
+        <label class="block text-2xl font-bold">Wrire Your Note:</label>
+        <textarea class="w-3xl h-32 p-2 text-left align-top resize-none border border-black rounded" placeholder="Write here.."></textarea>        
+        <div class="text-center mt-4">
+            <input type="submit" value="Submit Note" class="bg-blue-500 border border-black text-white p-3 rounded-lg">
+        </div>
+    </form>
 </div>
 
 <?php require('template/footer.php') ?>
