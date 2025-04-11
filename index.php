4dbe3d306725c0e32d57d1a443ca0a1f74786e48
Yousef <yousefmokhariq@gmail.com>
2025-04-11 13:57:32 +0300

    added header and footer

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require('template/header.php') ?>
+<div class="justify-center items-center h-screen">
+    <textarea></textarea>
+</div>
+
+<?php require('template/footer.php') ?>
+
+</html>
