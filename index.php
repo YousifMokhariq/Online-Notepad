3d522ec175e91a44b56389a88f6bd56ec4e0172d
Yousef <yousefmokhariq@gmail.com>
2025-04-23 16:01:13 +0300

    connected to the DB

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
-    $errors = array('note'=>'');
-    $display = array('note'=>'');
-    if (isset($_POST['submit'])){
 
-        if(!empty($_POST['note'])){
-        $display['note'] = htmlspecialchars($_POST['note']);
+$conn = mysqli_connect("localhost","yousif","test12345","onlinenotepad");
 
-    }else{
-        $errors['note'] = "You Have To Write A Note*";
-    }
+if(!$conn){
+    echo "Connection Fail: " . mysqli_connect_error();
+}
+
+$sql = "SELECT id, note FROM mynotes ORDER BY created_at";
+
+$result = mysqli_query($conn, $sql);
+
+$notes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
+
+$errors = array('note'=>'');
+$display = array('note'=>'');
+if (isset($_POST['submit'])){
+
+    if(!empty($_POST['note'])){
+    $display['note'] = htmlspecialchars($_POST['note']);
+
+}else{
+    $errors['note'] = "You Have To Write A Note*";
+}
 }
 
 ?>
@@ -25,11 +43,17 @@
         <div class="text-center mt-4">
             <input type="submit" name="submit" value="Submit Note" class="bg-blue-500 border border-black text-white p-3 rounded-lg">
         </div>
-
-        <div class="flex flex-wrap text-2xl text-blue-600 bg-gray-400 rounded-lg px-3 py-2"><?php echo $display['note'] ?></div>
     </form> 
 
+    <?php foreach($notes as $note){ ?>
+    <div>
+        <div>
+            <div class="text-2xl font-semibold"><?php echo htmlspecialchars($note['id']); echo htmlspecialchars($note['note']); echo htmlspecialchars($note['created_at']);?></div>
+        </div>
+    </div>
+    <?php } ?>
 </div>
+
 <?php require('template/footer.php') ?>
 
 </html>
--- mynotes.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php require('template/header.php') ?>
-
-<?php require('template/footer.php') ?>
-
-</html>
